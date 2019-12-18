f306f6c0accad96e37f8669f6b7d45b9bb2d8009
Hui <wang0x20hui@gmail.com>
2019-12-18 15:12:47 +0800

    try set but failed

--- 001_TwoSum/TwoSum.cxx
+++ 001_TwoSum/TwoSum.cxx
@@ -1,16 +1,15 @@
 #include<vector>
 #include<unordered_map>
 
-
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> Map;
+        std::unordered_map<int, int> HashMap;
         for (int i = 0; i != nums.size(); ++i) {
-            auto found = Map.find(nums[i]);
-            if (found != Map.end())
+            auto found = HashMap.find(nums[i]);
+            if (found != HashMap.end())
                 return {found->second, i};
-            Map.emplace(target - nums[i], i);
+            HashMap.emplace(target - nums[i], i);
         }
         return {-1, -1};
     }
