ff60ba86639129521f05befa10f1141831f8286e
Hui <wang0x20hui@gmail.com>
2019-12-18 23:53:28 +0800

    003 Add ignore words version

--- 003_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cxx
+++ 003_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cxx
@@ -21,3 +21,23 @@ public:
         return ans;
     }
 };
+
+
+// ignore words just concern about the index version 
+class Solution {
+public:  
+   int lengthOfLongestSubstring(string s) {
+        int  size,i=0,j,k,max=0;
+        size = s.size();
+        for(j = 0;j<size;j++){
+            for(k = i;k<j;k++)
+                if(s[k]==s[j]){
+                    i = k+1;
+                    break;
+                }
+            if(j-i+1 > max)
+                max = j-i+1;
+        }
+        return max;
+    }
+};
