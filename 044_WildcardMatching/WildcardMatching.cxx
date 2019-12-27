568968387c6c116211b2d9f4340fcb6ce644fd38
Hui <wang0x20hui@gmail.com>
2019-12-28 00:09:04 +0800

    044 add range for based fast than index for base

--- 044_WildcardMatching/WildcardMatching.cxx
+++ 044_WildcardMatching/WildcardMatching.cxx
@@ -88,6 +88,35 @@ public:
     }
 };
 
+class Solution3 {
+public:
+    bool isMatch(string s, string p) {                
+        vector< vector<bool> > dp(s.size()+1, vector<bool>(p.size()+1, false));
+        dp[0][0] = true;
+        // str "" condition
+        for( auto && pattern: p){
+            if( pattern == '*') dp[0][ distance(&p[0], &pattern)+1 ] = true;
+            else break;
+        }
+        for(auto && str : s){
+            int s_index = distance(&s[0], &str)+1;  
+            for(auto && pattern : p){
+                int p_index = distance(&p[0], &pattern)+1;
+                if(pattern == '*'){
+                    dp[s_index][p_index] = dp[s_index-1][p_index] || dp[s_index][p_index-1];  
+                }
+                else if( pattern == str || pattern == '?'){
+                    dp[s_index][p_index] = dp[s_index-1][p_index-1];
+                }
+                else ;
+                ++p_index;
+            }
+            ++s_index;
+        }
+        return  dp[s.size()][p.size()];
+    }
+};
+
 int main(int argc, char * argv[]){
     string a = "aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba";
     string b = "a*******b";
