e4c9dd41956551159af690f21815da5aa94a517d
Hui <wang0x20hui@gmail.com>
2020-01-02 21:40:00 +0800

    024 Modify

--- 024_SwapNodesinPairs/SwapNodesinPairs.cxx
+++ 024_SwapNodesinPairs/SwapNodesinPairs.cxx
@@ -6,6 +6,12 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
