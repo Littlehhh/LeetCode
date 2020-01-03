729d150b9ac6fbf6db19c5743757fd0881744965
Hui <wang0x20hui@gmail.com>
2020-01-03 15:23:31 +0800

    024 Add new solution

--- 024_SwapNodesinPairs/SwapNodesinPairs.cxx
+++ 024_SwapNodesinPairs/SwapNodesinPairs.cxx
@@ -6,7 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
+// Recursion version
 struct ListNode {
     int val;
     ListNode *next;
@@ -16,21 +16,52 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(head == nullptr || head->next == nullptr) return head;
-        //  cur  ->  tmp
         ListNode * cur = head;
-        ListNode * tmp = nullptr;
-        
-        tmp = cur->next;
+        ListNode * tmp = cur->next;
         cur->next = tmp->next;
         tmp->next = cur;
         head = tmp;
-        
-        while(cur->next){
-            tmp = cur->next;
+        cur->next = swapPairs(cur->next);
+        return head;
+    }
+};
+
+
+// Recursion version
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+// optim
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode * newHead = head->next;
+        head->next = newHead->next;
+        newHead->next = head;
+        head->next = swapPairs(head->next);
+        return newHead;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode * cur = head;
+        head = head->next;
+        ListNode * pre = new ListNode(-1);
+        while(cur && cur->next){
+            ListNode * tmp = cur->next;
             cur->next = tmp->next;
             tmp->next = cur;
+            pre->next = tmp;
+            pre = cur;
             cur = cur->next;
-        }
+        }        
         return head;
     }
 };
