7788ce1fd5dfd06314fefe0638873fa735d8c779
Hui <wang0x20hui@gmail.com>
2020-01-08 21:54:45 +0800

    028 Add

--- 028_ImplementstrStr/ImplementstrStr.cxx
+++ 028_ImplementstrStr/ImplementstrStr.cxx
@@ -31,3 +31,23 @@ public:
         return -1;
     }
 };
+
+// 暴力法
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        for(int i = 0; i < n - m + 1; i ++)
+        {
+            int j = 0;
+            for( ; j < m; j ++)
+            {
+                if(haystack[i + j] !=  needle[j])
+                    break;
+            }
+            if(j == m)
+                return i;
+        }
+        return -1;
+    }
+};
