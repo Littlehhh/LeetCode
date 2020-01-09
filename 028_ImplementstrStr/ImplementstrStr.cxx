62368e635282aabd9a74cf61f69b2313eedac7ad
Hui <wang0x20hui@gmail.com>
2020-01-09 12:49:27 +0800

    028 add

--- 028_ImplementstrStr/ImplementstrStr.cxx
+++ 028_ImplementstrStr/ImplementstrStr.cxx
@@ -1,5 +1,6 @@
 #include<string>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 // over time limits
@@ -9,10 +10,8 @@ public:
         if(needle.size() == 0) return 0;
         if(haystack.size() == 0) return -1;
         vector< vector<bool> > dp(haystack.size()+1, vector<bool>(needle.size()+1, false));
-        
         for( int i = 0; i <= haystack.size(); ++i)
             dp[i][0] = true;
-        
         for(int i = 0; i < needle.size(); ++i ){
             bool exist = false;
             for (int j = 0; j < haystack.size(); ++j){
@@ -32,7 +31,7 @@ public:
     }
 };
 
-// 暴力法
+// BF 暴力
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -49,5 +48,41 @@ public:
                 return i;
         }
         return -1;
+    }
+};
+
+// sunday
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size() == 0) return 0;
+        if(haystack.size() == 0
+        || haystack.size() < needle.size()) return -1;
+        int m = needle.size();
+        unordered_map<char, int> offset;
+        for(int i=m-1; i >=0 ; --i){
+            if( offset.find(needle[i]) == offset.end()){
+                offset.insert({needle[i], m-i});
+            }
+        }
+
+        int i = 0;
+        for(; i <= haystack.size() - m; ){
+            if( haystack.substr(i, m) == needle)
+                return i;
+            else{
+                if (i+m >= haystack.size())
+                    return -1;
+
+                auto found = offset.find(haystack[i+m]);
+                if(found != offset.end()){
+                    i += found->second;
+                }
+                else{
+                    i += (m + 1);
+                }
+            }
+        }
+        return  i+m < haystack.size()? i : -1;
     }
 };
