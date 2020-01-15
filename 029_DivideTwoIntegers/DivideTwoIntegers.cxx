c9ae363ce9f5bf4f175752ba5a5e0a6ef3ee24de
Hui <wang0x20hui@gmail.com>
2020-01-16 00:39:06 +0800

    029 Modify

--- 029_DivideTwoIntegers/DivideTwoIntegers.cxx
+++ 029_DivideTwoIntegers/DivideTwoIntegers.cxx
@@ -1,4 +1,4 @@
-#include<vector>
+#include<climits>
 
 using namespace std;
 class Solution {
@@ -6,17 +6,22 @@ public:
     int divide(int dividend, int divisor) {
         if(dividend == 0 || divisor == 0) return 0;
         if(divisor == INT_MIN) return dividend == INT_MIN ? 1:0;
+        if(divisor == -1) return dividend == INT_MIN ? INT_MAX : -dividend; 
+        if(divisor == 1) return dividend; 
         bool sign = dividend > 0 == divisor > 0; 
-        if(dividend > 0) -dividend; 
-        if(divisor < 0 ) -divisor;
-        vector<int> divisors;
-        divisors.clear();
+        if(dividend > 0) dividend = -dividend; 
+        if(divisor < 0) divisor = -divisor;
         int ans = 0;
         while(-divisor >= dividend){ // because negetive
-            divisors.push_back(divisor);
-            cout << divisor << " ";
-            divisor <<= 1;
+            int tmp = divisor;
+            int i = 1;
+            while( tmp<<1 > 0 && -(tmp<<1) >= dividend){
+                tmp <<= 1;
+                i <<= 1;
+            }
+            ans += i;
+            dividend += tmp; 
         }
-        return ans;
+        return sign? ans:-ans;
     }
 };
