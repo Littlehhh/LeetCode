ec4112252218bb88f312708743c47225e24c3686
Hui <wang0x20hui@gmail.com>
2020-01-15 23:04:11 +0800

    029 Modify

--- 029_DivideTwoIntegers/DivideTwoIntegers.cxx
+++ 029_DivideTwoIntegers/DivideTwoIntegers.cxx
@@ -1,6 +1,22 @@
+#include<vector>
+
+using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        
+        if(dividend == 0 || divisor == 0) return 0;
+        if(divisor == INT_MIN) return dividend == INT_MIN ? 1:0;
+        bool sign = dividend > 0 == divisor > 0; 
+        if(dividend > 0) -dividend; 
+        if(divisor < 0 ) -divisor;
+        vector<int> divisors;
+        divisors.clear();
+        int ans = 0;
+        while(-divisor >= dividend){ // because negetive
+            divisors.push_back(divisor);
+            cout << divisor << " ";
+            divisor <<= 1;
+        }
+        return ans;
     }
 };
