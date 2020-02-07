d94129d6ba494b3f09f369f2574ec54edf29a0d0
Hui <wang0x20hui@gmail.com>
2020-02-07 16:46:21 +0800

    048 Add

---
+++ 048_RotateImage/RotateImage.cxx
@@ -0,0 +1,20 @@
+#include<vector>
+using std::vector;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        if(matrix.size() != matrix[0].size()) return;
+        int n = matrix.size();
+        for(int i = 0; i < (n+1)/2; ++i){
+            for(int j = 0; j < n/2; ++j){
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+
+            }
+        }
+    }
+};
