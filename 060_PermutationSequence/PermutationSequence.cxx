d10e2bd7eb36a63fc3c837d372e0a379248a5bdd
Hui <wang0x20hui@gmail.com>
2020-03-03 14:22:00 +0800

    060 Add cantor encode

--- 060_PermutationSequence/PermutationSequence.cxx
+++ 060_PermutationSequence/PermutationSequence.cxx
@@ -1,8 +1,10 @@
 #include<string>
-#include <algorithm>
+#include<algorithm>
+#include<vector>
 using std::string;
+using std::vector;
 
-class Solution {
+class Solution1 {
 public:
     string getPermutation(int n, int k) {
         string s = string("123456789").substr(0,n);
@@ -14,7 +16,7 @@ public:
     
 };
 
-// Possible implementation
+// Possible implementation cpp doc
 template<class BidirIt>
 bool next_permutation(BidirIt first, BidirIt last)
 {
@@ -40,3 +42,32 @@ bool next_permutation(BidirIt first, BidirIt last)
         }
     }
 }
+
+// cantor encode decode
+class Solution {
+    vector<int> fac;
+public:
+    string getPermutation(int n, int k) {
+        string ans;
+        string s = string("123456789").substr(0, n);
+        generateFac(n);
+        --k;
+        while(k > 0)
+        {  
+            size_t i = k/fac[n - 1];
+            ans.push_back(s[i]);
+            s.erase(s.begin() + i);
+            k %= fac[n - 1];
+            --n;
+        }
+        return ans + s;
+    }
+    void generateFac(int n){
+        fac.emplace_back(1);
+        fac.emplace_back(1);
+        for(int i = 2; i < n; ++i){
+            fac.emplace_back( i * fac[i-1] );
+        }
+        return;
+    }
+};
