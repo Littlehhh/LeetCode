32140d5160dafce05826b1435048709a7a5760ea
Hui <wang0x20hui@gmail.com>
2020-03-07 14:20:35 +0800

    094 Add

---
+++ 094_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cxx
@@ -0,0 +1,28 @@
+#include<cstddef>
+#include<vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root == nullptr) return ans;
+        Traversal(root);
+        return ans;
+    }
+    void Traversal(TreeNode * root){
+        if(root == nullptr) return ;
+        if(root->left != nullptr) Traversal(root->left);
+        ans.push_back(root->val);
+        if(root->right != nullptr) Traversal(root->right);
+    }
+};
