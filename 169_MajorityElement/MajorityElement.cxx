c34c41b40e0dfdca2316a5e8bec901dadc782c0c
Hui <wang0x20hui@gmail.com>
2020-03-13 15:54:56 +0800

    :sparkles: 169 Add Moore voting

--- 169_MajorityElement/MajorityElement.cxx
+++ 169_MajorityElement/MajorityElement.cxx
@@ -2,7 +2,7 @@
 #include<unordered_map>
 
 using namespace std;
-class Solution {
+class Solution1 {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> table;
@@ -18,5 +18,21 @@ public:
             }
         }
         return max_index;
+    }
+};
+
+// Moore voting
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {    
+        int count=0,candidate;
+        for(int n:nums)
+        {
+            if(count==0)        candidate=n;
+
+            if(n==candidate)    count++;
+            else                count--;
+        }
+        return candidate;
     }
 };
