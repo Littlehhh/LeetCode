1b27c6c144b3058e81369decab69d9dbcf012f59
Hui <wang0x20hui@gmail.com>
2020-03-31 16:51:24 +0800

    :sparkles: 912 Add

---
+++ 912_SortanArray/SortanArray.cxx
@@ -0,0 +1,11 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
