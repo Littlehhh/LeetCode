b4d6cf98297210f42578e6bfbaab94bbc798ae8b
Hui <wang0x20hui@gmail.com>
2020-03-31 19:22:19 +0800

    :sparkles: 912 Add counting sort

--- 912_SortanArray/SortanArray.cxx
+++ 912_SortanArray/SortanArray.cxx
@@ -8,4 +8,22 @@ public:
         sort(nums.begin(), nums.end());
         return nums;
     }
+    void CountingSort(vector<int>& nums){
+        int min = INT_MAX, max = INT_MIN;
+        for(auto && num:nums){
+            min = std::min(num, min);
+            max = std::max(num, max);
+        }
+        vector<int> counter(max-min+1);
+        for (int num: nums) {
+            counter[num - min]++;
+        }
+        int idx = 0;
+        for (int num = min; num <= max; num++) {
+            int cnt = counter[num - min];
+            while (cnt-- > 0) {
+                nums[idx++] = num;
+            }
+        }
+    }
 };
