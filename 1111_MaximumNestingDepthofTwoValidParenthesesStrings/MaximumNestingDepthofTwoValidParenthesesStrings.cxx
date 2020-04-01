e5a871efa7a0384ce922481e62ad606bbbc39f5f
Hui <wang0x20hui@gmail.com>
2020-04-01 14:33:53 +0800

    :sparkles: 1111 Modify

--- 1111_MaximumNestingDepthofTwoValidParenthesesStrings/MaximumNestingDepthofTwoValidParenthesesStrings.cxx
+++ 1111_MaximumNestingDepthofTwoValidParenthesesStrings/MaximumNestingDepthofTwoValidParenthesesStrings.cxx
@@ -7,7 +7,7 @@ class Solution {
 public:
     vector<int> maxDepthAfterSplit(string seq) {
         vector<int> ans;
-        for (int i = 0; i < (int)seq.size(); ++i) {
+        for (int i = 0; i < seq.size(); ++i) {
             ans.push_back(i & 1 ^ (seq[i] == '('));
         }
         return ans;
