d1d24270cdab1c3c61d45e7a54418c16375985a0
Hui <wang0x20hui@gmail.com>
2020-04-20 11:00:01 +0800

    :sparkles: 200 Modify

--- 200_NumberofIslands/NumberofIslands.cxx
+++ 200_NumberofIslands/NumberofIslands.cxx
@@ -52,7 +52,7 @@ private:
     int count;
 };
 
-class Solution {
+class Solution1 {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int nr = grid.size();
@@ -74,5 +74,43 @@ public:
         }
 
         return uf.getCount();
+    }
+};
+
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j)
+    {
+        int nr=grid.size();
+        int nc=grid[0].size();
+        grid[i][j] = '0';
+
+        if((i-1)>=0 && grid[i-1][j] == '1') dfs(grid, i-1, j);
+        if((i+1)<nr && grid[i+1][j] == '1') dfs(grid, i+1, j);
+        if((j-1)>=0 && grid[i][j-1] == '1') dfs(grid, i, j-1);
+        if((j+1)<nc && grid[i][j+1] == '1') dfs(grid, i, j+1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int nr=grid.size();
+        if (!nr) return 0;
+        int nc=grid[0].size();
+
+        int numlands=0;
+
+        for(int i=0;i<nr;i++)
+        {
+            for(int j=0;j<nc;j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    numlands++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+
+        return numlands;
+
     }
 };
