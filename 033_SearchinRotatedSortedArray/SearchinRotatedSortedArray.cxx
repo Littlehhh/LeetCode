d06f5b7905f1609c4f535efcd6307230f1841d28
Hui <wang0x20hui@gmail.com>
2020-04-27 13:51:56 +0800

    :sparkles: 033 Modify

--- 033_SearchinRotatedSortedArray/SearchinRotatedSortedArray.cxx
+++ 033_SearchinRotatedSortedArray/SearchinRotatedSortedArray.cxx
@@ -1,7 +1,8 @@
 #include<vector>
 using namespace std;
 
-class Solution {
+// normal binary search style
+class Solution1 {
 public:
     int search(vector<int>& nums, int target) {
         if(nums.size() == 0) return -1;
@@ -27,27 +28,30 @@ public:
 };
 
 
+// iterator binary search style
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         if(nums.size() == 0) return -1;
         if(nums.size() == 1) return target == nums[0]? 0 : -1;
         auto &&lo = nums.begin(), &&hi = nums.end();      
+        auto &&mid = vector<int>::iterator();
+        // binary search the rotated pivot
         if( *nums.begin() > *nums.rbegin()){
             while(lo != hi){
-                auto &&mid = lo + distance(lo, hi) / 2;
+                mid = lo + distance(lo, hi) / 2;
                 if( *mid > *nums.rbegin() ) lo = mid + 1;
                 else hi = mid;
             }
             if( target < nums[0] ) hi = nums.end();
             else lo = nums.begin();
         }
-        while( lo != hi ){
-            auto &&mid = lo + distance(lo, hi) / 2;            
+        // binary search the target
+        while( lo != hi ){           
+            mid = lo + distance(lo, hi) / 2;
             if( *mid < target ) lo = mid + 1;
-            else hi = mid;       
+            else hi = mid;      
         }
-        if( lo != nums.end() && target == *lo ) return distance(nums.begin(),lo);
-        else return -1;
+        return lo != nums.end() && target == *lo ? distance(nums.begin(),lo) : -1;
     }
 };
