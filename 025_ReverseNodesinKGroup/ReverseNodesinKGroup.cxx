8382bbf50acd20413f31f88eeddbbf10d8fda503
Hui <wang0x20hui@gmail.com>
2020-05-16 00:26:15 +0800

    :sparkles: 025 review

--- 025_ReverseNodesinKGroup/ReverseNodesinKGroup.cxx
+++ 025_ReverseNodesinKGroup/ReverseNodesinKGroup.cxx
@@ -1,27 +1,64 @@
+#include<stack>
+using namespace std;
+
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-class Solution {
+class Solution1 {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode * newHead = head;
         ListNode * cur = head;
         int n = k;
+        // find new head
         while(--n && newHead){
             newHead = newHead->next;
         }
         if(n > 0 || newHead == nullptr) return head;
         n=k;
+        // reverse one group
         while(--n){
             ListNode * tmp = cur;
             cur = cur->next;
             tmp->next = newHead->next;
             newHead->next = tmp;
         }
+        // now the head is tail and than reverse remain groups
         head->next = reverseKGroup(head->next, k);
         return newHead;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        stack<ListNode*> s;
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* tmp = head, *p = dummy;
+        while (true){
+            int count = 0;
+            ListNode* tmphead = tmp;
+            while (tmp && count < k ){
+                s.push(tmp);
+                tmp = tmp->next;
+                ++count;
+            }
+            if (count < k) {
+                p->next = tmphead;
+                break;
+            }
+            while (!s.empty()){
+                p->next = s.top();
+                s.pop();
+                p = p->next;
+                p->next = nullptr;
+            }
+        }
+        return dummy->next;
     }
 };
