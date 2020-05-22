0cae8de47e2d4949a30e06472a2640adade7709b
Hui <wang0x20hui@gmail.com>
2020-05-22 18:49:27 +0800

    :sparkles: 071 Add

---
+++ 071_SimplifyPath/SimplifyPath.cxx
@@ -0,0 +1,26 @@
+#include<sstream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream is(path);
+        vector<string> strs;
+        string res = "", tmp = "";
+        while(getline(is, tmp, '/')) {
+            if(tmp == "" || tmp == ".")
+                continue;
+            else if(tmp == ".." && !strs.empty())
+                strs.pop_back();
+            else if(tmp != "..")
+                strs.push_back(tmp);
+        }
+        for(string str:strs) 
+            res +=  "/" + str;
+        if(res.empty())
+            return "/";
+        return res;
+    }
+};
