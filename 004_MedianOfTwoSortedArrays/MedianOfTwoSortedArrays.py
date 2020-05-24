9d6129d4deca73a57ba231659982a5af88d307f4
Hui <wang0x20hui@gmail.com>
2020-05-24 15:46:02 +0800

    :sparkles: 004 Add python version

--- 004_MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.py
+++ 004_MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.py
@@ -20,3 +20,30 @@ class Solution:
             return (All[int(count/2)]+All[int(count/2)-1])/2
         else:
             return float(All[int(count/2)])
+
+#  O(log(min(m,n)))
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            # nums1, nums2 = nums2, nums1
+            return self.findMedianSortedArrays(nums2, nums1)
+        total_mid = (len(nums1)+len(nums2)+1) // 2
+        
+        lo, hi = 0, len(nums1)
+        while lo < hi:
+            mid = lo+ (hi-lo) // 2
+            remain = total_mid - mid
+            if nums1[mid] < nums2[remain-1]:
+                lo = mid+1
+            else:
+                hi = mid
+        
+        mid, remain = lo, total_mid-lo
+        
+        max_left = max( float('-inf') if mid <= 0 else nums1[mid-1],
+                        float('-inf') if remain <= 0 else nums2[remain-1])
+        if (len(nums1)+len(nums2)) & 1:
+            return max_left
+        min_right = min( float('inf') if mid >= len(nums1)  else nums1[mid],
+                        float('inf') if remain >= len(nums2) else nums2[remain])
+        return (min_right+max_left) / 2
