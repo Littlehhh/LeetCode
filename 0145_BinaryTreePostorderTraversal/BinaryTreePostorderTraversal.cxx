a1806809fb449133fbdbde2412f0c6ab090af34e
Hui <wang0x20hui@gmail.com>
2020-06-04 15:08:22 +0800

    :sparkles: 145 Add

---
+++ 0145_BinaryTreePostorderTraversal/BinaryTreePostorderTraversal.cxx
@@ -0,0 +1,26 @@
+#include<vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        if(!root) ;
+        else{
+            postorderTraversal(root->left);
+            postorderTraversal(root->right);
+            ans.push_back(root->val);
+        }
+        return ans;
+    }
+};
