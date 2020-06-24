ce2a59e7b84f92de97a0254836b33cf5a860f8aa
Hui <wang0x20hui@gmail.com>
2020-06-24 10:08:45 +0800

    :sparkles: 0016 Add

---
+++ 0016_3SumClosest/3SumClosest.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        Min = inf
+        ans = inf
+        for i in range(len(nums)):
+            lo = i+1;
+            hi = len(nums)-1
+            while lo < hi:
+                tmp=0
+                S = nums[i]+nums[lo]+nums[hi];
+                if S>target:
+                    tmp = S-target
+                    hi-=1
+                elif S<target:
+                    tmp = target-S
+                    lo+=1
+                elif S == target:
+                    return target
+                if tmp < Min:
+                    Min=tmp
+                    ans=S
+        return ans
