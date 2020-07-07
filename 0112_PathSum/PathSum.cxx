b43e87d2a6aeef9b4ad3c546145aef6caa0064ef
Hui <wang0x20hui@gmail.com>
2020-07-07 08:00:34 +0800

    :sparkles: 0112 Add

---
+++ 0112_PathSum/PathSum.cxx
@@ -0,0 +1,20 @@
+#include<cstddef>
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root) return false;
+        if(!root->left && !root->right) return sum == 0;
+        
+        return 
+        hasPathSum(root->left, sum-root->val) ||
+        hasPathSum(root->right, sum-root->val);
+    }
+};
